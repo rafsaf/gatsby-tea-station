d5d9884190609b30d3bc7b4f5ed2b6882eddfaae
rafsaf <rafal.safin12@gmail.com>
2021-03-18 14:52:13 +0100

    added 16,32,192,512px favicons

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,28 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icons: [
+          {
+            src: `src/images/favicon-16x16.png`,
+            sizes: `16x16`,
+            type: `image/png`,
+          },
+          {
+            src: `src/images/favicon-32x32.png`,
+            sizes: `32x32`,
+            type: `image/png`,
+          },
+          {
+            src: `src/images/favicon-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `src/images/favicon-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
       },
     },
     "gatsby-plugin-sharp",
